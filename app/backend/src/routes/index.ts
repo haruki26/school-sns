ff95af63fc9fa5de4204b427d515464230bed9c1
haruki26 <h826.a519@gmail.com>
2026-01-11 17:14:02 +0000

    remove: デモ定義を削除

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,65 +1,19 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { describeRoute, resolver, validator } from 'hono-openapi'
-import z from 'zod'
 import { artifacts } from './artifacts/index.js'
 import { auth } from './auth/index.js'
 import { media } from './media/index.js'
-import { demoQuerySchema, demoResponseSchema } from './schema.js'
 import { scraps } from './scraps/index.js'
 import { search } from './search/index.js'
 import { users } from './users/index.js'
 
 export const app = new Hono()
+  .basePath('/api/v2')
   .use(
     '*',
     cors({
       origin: ['http://localhost:3157'],
     }),
-  )
-  .get(
-    '/',
-    describeRoute({
-      description: 'Say Hello from Hono',
-      responses: {
-        200: {
-          description: 'Successful Response',
-          content: {
-            'text/plain': {
-              schema: resolver(z.string()),
-            },
-          },
-        },
-      },
-    }),
-    (c) => {
-      return c.text('Hello Hono!')
-    },
-  )
-  .get(
-    '/demo',
-    describeRoute({
-      description: 'Demo endpoint',
-      responses: {
-        200: {
-          description: 'Successful Response',
-          content: {
-            'application/json': {
-              schema: resolver(demoResponseSchema),
-            },
-          },
-        },
-      },
-    }),
-    validator('query', demoQuerySchema),
-    (c) => {
-      const { kind } = c.req.valid('query')
-      if (kind === 'positive') {
-        return c.json({ message: 'This is a positive message! 😊' })
-      } else {
-        return c.json({ message: 'This is a negative message! 😞' })
-      }
-    },
   )
   .route('/auth', auth)
   .route('/users', users)
