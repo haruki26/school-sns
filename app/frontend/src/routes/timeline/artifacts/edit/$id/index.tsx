eb81722054e5054a1d2d36bad23c0e0fbf2c89ab
haruki26 <h826.a519@gmail.com>
2026-02-04 15:50:18 +0000

    add: 記事編集ページを作成

--- app/frontend/src/routes/timeline/artifacts/edit/$id/index.tsx
+++ app/frontend/src/routes/timeline/artifacts/edit/$id/index.tsx
@@ -1,5 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useFetchTagsOptions } from '@/api/routes/tags'
+import { useFetchSelfInfoOptions } from '@/api/routes/users'
+import { useFetchArtifactsDetailOptions } from '@/api/routes/artifacts'
+import { UneditableError } from '@/features/timeline/errors'
 
 export const Route = createFileRoute('/timeline/artifacts/edit/$id/')({
-  loader: () => console.log('Loading /timeline/artifacts/edit/$id/ route'),
+  loader: async ({ context: { queryClient }, params }) => {
+    queryClient.ensureQueryData(useFetchTagsOptions())
+
+    const {
+      user: { id: targetUserId },
+    } = await queryClient.ensureQueryData(
+      useFetchArtifactsDetailOptions(params.id),
+    )
+    const { id: accessUserId } = await queryClient.ensureQueryData(
+      useFetchSelfInfoOptions(),
+    )
+
+    if (targetUserId !== accessUserId) {
+      throw new UneditableError('artifact')
+    }
+  },
 })
