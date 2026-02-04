70220321cbade1f2d8d66f07a4b09bf10b838e6c
haruki26 <h826.a519@gmail.com>
2026-02-04 01:47:02 +0000

    add: エディターの作成

---
+++ app/frontend/src/features/timeline/components/Editor/index.tsx
@@ -0,0 +1,51 @@
+import '@blocknote/core/fonts/inter.css'
+import { BlockNoteView } from '@blocknote/mantine'
+import '@blocknote/mantine/style.css'
+import { useCreateBlockNote } from '@blocknote/react'
+import { ja } from '@blocknote/core/locales'
+import { useEffect } from 'react'
+import type React from 'react'
+import { cn } from '@/utils/cn'
+
+interface Props {
+  initialContent: string | null
+  onChange: (content: string) => void
+  className?: string
+}
+
+const Editor: React.FC<Props> = ({
+  initialContent = null,
+  onChange,
+  className,
+}) => {
+  const editor = useCreateBlockNote({
+    dictionary: ja,
+  })
+
+  useEffect(() => {
+    if (initialContent === null) return
+    const blocks = editor.tryParseMarkdownToBlocks(initialContent)
+    editor.replaceBlocks(editor.document, blocks)
+  }, [editor])
+
+  const handleChange = () => {
+    const content = editor.blocksToMarkdownLossy(editor.document)
+    onChange(content)
+  }
+
+  return (
+    <BlockNoteView
+      editor={editor}
+      onChange={handleChange}
+      formattingToolbar
+      theme="light"
+      sideMenu
+      spellCheck
+      slashMenu
+      linkToolbar
+      className={cn('px-2 py-4 min-h-50', className)}
+    />
+  )
+}
+
+export default Editor
