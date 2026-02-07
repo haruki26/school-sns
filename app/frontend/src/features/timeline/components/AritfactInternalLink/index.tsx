e5a3529994e625c0942884e27ad1865795f9bd3b
haruki26 <h826.a519@gmail.com>
2026-02-07 07:08:00 +0000

    fix: aタグのネストを避けるためにbuttonでnavigationを発火する形に変更

--- app/frontend/src/features/timeline/components/AritfactInternalLink/index.tsx
+++ app/frontend/src/features/timeline/components/AritfactInternalLink/index.tsx
@@ -1,5 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { Link } from '@tanstack/react-router'
+import { useNavigate } from '@tanstack/react-router'
 import { useFetchArtifactsDetailOptions } from '@/api/routes/artifacts'
 import IconWithLabel from '@/components/ui/IconWithLabel'
 import Avatar from '@/components/ui/Avatar'
@@ -10,31 +10,38 @@ interface Props {
 
 const ArtifactInternalLink: React.FC<Props> = ({ artifactId }) => {
   const { data } = useSuspenseQuery(useFetchArtifactsDetailOptions(artifactId))
+  const navigate = useNavigate()
 
   return (
-    <Link
-      to="/timeline/artifacts/detail/$id"
-      params={{ id: artifactId }}
-      className="w-full rounded-xl border border-slate-800 px-2 py-5 block"
+    <button
+      onClick={(e) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        navigate({
+          to: '/timeline/artifacts/detail/$id',
+          params: { id: artifactId },
+        })
+      }}
+      type="button"
+      className="w-full rounded-xl border border-slate-800 px-3 py-2 block"
     >
-      <div className="flex flex-col gap-3 text-black">
-        <div className="flex items-center gap-3">
-          <IconWithLabel
-            icon={() => (
-              <Avatar
-                src={data.user.avatarUrl ?? undefined}
-                alt={data.user.userName}
-                className="w-6 h-6"
-              />
-            )}
-            label={() => (
-              <span className="font-bold text-md">{data.user.userName}</span>
-            )}
-          />
-        </div>
+      <div className="flex flex-col items-start gap-1 text-black">
+        <IconWithLabel
+          icon={() => (
+            <Avatar
+              src={data.user.avatarUrl ?? undefined}
+              alt={data.user.userName}
+              className="w-6 h-6"
+            />
+          )}
+          label={() => (
+            <span className="font-bold text-md">{data.user.userName}</span>
+          )}
+        />
         <div className="font-bold text-lg">{data.title}</div>
       </div>
-    </Link>
+    </button>
   )
 }
 
